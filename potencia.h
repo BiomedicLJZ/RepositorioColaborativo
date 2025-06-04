9027d27b79862b4356dee3fd2a14db439dfdc975
Raziel Prado <razieloswaldo8@gmail.com>
2025-06-04 09:30:00 -0600

    minor changes

---
+++ potencia.h
@@ -0,0 +1,13 @@
+// potencia.h
+
+#ifndef POTENCIA_H
+#define POTENCIA_H
+
+// Calcula la base elevado al exponente
+int potencia(int base, int exponente);
+
+// Para números flotantes
+double potencia_double(double base, double exponente);
+
+#endif // POTENCIA_H
+
